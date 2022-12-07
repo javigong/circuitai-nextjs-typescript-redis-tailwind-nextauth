7130def2caea96525d88b8a8772796276c897484
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-07 14:55:47 -0800

    Create MessageList.tsx

---
+++ app/MessageList.tsx
@@ -0,0 +1,18 @@
+type Props = {};
+
+const MessageList = (props: Props) => {
+  return (
+    <div>
+      <div>Messages go here...</div>
+      <div>Messages go here...</div>
+      <div>Messages go here...</div>
+      <div>Messages go here...</div>
+      <div>Messages go here...</div>
+      <div>Messages go here...</div>
+      <div>Messages go here...</div>
+      <div>Messages go here...</div>
+    </div>
+  );
+};
+
+export default MessageList;
