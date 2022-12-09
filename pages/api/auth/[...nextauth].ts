a34161432eb47b2ff527cfcf8886ca574c0f458c
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-08 21:08:42 -0800

    Create [...nextauth].ts

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.SECRET,
+  pages: {
+    signIn: "/auth/signin",
+  }
+};
+
+export default NextAuth(authOptions);
