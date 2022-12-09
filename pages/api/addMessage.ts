3d64f74eb5481c3417695c812a7c9d5e77f3e700
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-08 21:08:41 -0800

    Create addMessage.ts

---
+++ pages/api/addMessage.ts
@@ -0,0 +1,37 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { serverPusher } from "../../pusher";
+import redis from "../../redis";
+
+import { IMessage } from "../../types/typings";
+
+type Data = {
+  message: IMessage;
+};
+
+type ErrorData = {
+  body: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | ErrorData>
+) {
+  if (req.method !== "POST") {
+    res.status(405).json({ body: "Method not allowed" });
+    return;
+  }
+
+  const { message } = req.body;
+
+  const newMessage = {
+    ...message,
+    created_at: Date.now(),
+  };
+
+  // Push the new message to Redis (Upstash)
+  await redis.hset("messages", message.id, JSON.stringify(newMessage));
+  serverPusher.trigger("messages", "new-message", newMessage);
+
+  res.status(200).json({ message: newMessage });
+}
