d6c8d922a932570a74a4ce9c2e2a0878f8429eaf
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-08 21:08:24 -0800

    Update ChatInput.tsx

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -1,26 +1,71 @@
 "use client";
 
+import type { Session } from "next-auth";
 import { FormEvent, useState } from "react";
+import useSWR from "swr";
+import { v4 as uuid } from "uuid";
+import { IMessage } from "../types/typings";
+import fetchMessages from "../utils/fetchMessages";
 
-type Props = {};
+type Props = {
+  session: Session;
+};
 
-const ChatInput = (props: Props) => {
+const ChatInput = ({ session }: Props) => {
   const [input, setInput] = useState("");
+  const {
+    data: messages,
+    error,
+    mutate,
+  } = useSWR("/api/getMessages", fetchMessages);
 
-  const addMessage = (e: FormEvent<HTMLFormElement>) => {
+  console.log({ messages });
+
+  const addMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!input) return;
+
     const messageToSend = input;
     setInput("");
+
+    const id = uuid();
+    const message: IMessage = {
+      id,
+      message: messageToSend,
+      created_at: Date.now(),
+      username: session?.user?.name!,
+      profilePic: session?.user?.image!,
+      email: session?.user?.email!,
+    };
+
+    const uploadMessageToRedis = async () => {
+      const data = await fetch("/api/addMessage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message,
+        }),
+      }).then((res) => res.json());
+
+      return [data.message, ...messages!];
+    };
+
+    await mutate(uploadMessageToRedis, {
+      optimisticData: [message, ...messages!],
+      rollbackOnError: true,
+    });
   };
 
   return (
     <form
       onSubmit={addMessage}
-      className="fixed bottom-0 w-full z-50 flex px-10 py-5 space-x-2 border-t border-gray-100"
+      className="fixed bottom-0 w-full z-50 flex px-10 py-5 space-x-2 bg-white border-t border-gray-100"
     >
       <input
+        disabled={!session}
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
