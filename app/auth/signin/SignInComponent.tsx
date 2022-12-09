1eae41b7153b5a91087f5cf82d5dc64e1fd75bd7
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-08 21:08:22 -0800

    Create SignInComponent.tsx

---
+++ app/auth/signin/SignInComponent.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { getProviders, signIn } from "next-auth/react";
+
+type Props = {
+  providers: Awaited<ReturnType<typeof getProviders>>;
+};
+
+const SignInComponent = ({ providers }: Props) => {
+  return (
+    <div className="flex justify-center">
+      {Object.values(providers!).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#74E6DA] hover:bg-[#60beb5] text-white font-bold py-2 px-4 rounded"
+            onClick={() =>
+              signIn(provider.id, {
+                callbackUrl: process.env.VERCEL_URL || "http://localhost:3000",
+              })
+            }
+          >
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SignInComponent;
