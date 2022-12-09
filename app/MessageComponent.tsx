7bdf116abe441ef24198f2b76fa4f62c4dc0cce3
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-09 11:10:15 -0800

    Update MessageComponent.tsx

--- app/MessageComponent.tsx
+++ app/MessageComponent.tsx
@@ -1,5 +1,6 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import TimeAgo from 'react-timeago';
 import { IMessage } from "../types/typings";
 
 type Props = {
@@ -7,14 +8,14 @@ type Props = {
 };
 
 const MessageComponent = ({ message }: Props) => {
-  const {data: session} = useSession();
+  const { data: session } = useSession();
   const isUser = session?.user?.email === message.email;
 
   return (
     <div className={`flex w-fit ${isUser && "ml-auto"}`}>
       <div className={`flex-shrink-0 ${isUser && "order-2"}`}>
         <Image
-          className="rounded-full"
+          className="rounded-full mx-2"
           height={40}
           width={40}
           src={message.profilePic}
@@ -25,7 +26,7 @@ const MessageComponent = ({ message }: Props) => {
       <div>
         <p
           className={`text-[0.65rem] px-[2px] pb-[2px] ${
-            isUser ? "text-blue-400 text-right" : "text-[#44e4d4] text-left"
+            isUser ? "text-[#7C29E8] text-right" : "text-[#44e4d4] text-left"
           }`}
         >
           {message.username}
@@ -34,7 +35,7 @@ const MessageComponent = ({ message }: Props) => {
         <div className="flex items-end">
           <div
             className={`px-3 py-2 rounded-lg w-fit text-white ${
-              isUser ? "bg-blue-200 ml-auto order-2" : "bg-[#74E6DA]"
+              isUser ? "bg-[#7C29E8] ml-auto order-2" : "bg-[#74E6DA]"
             } `}
           >
             <p>{message.message}</p>
@@ -45,7 +46,7 @@ const MessageComponent = ({ message }: Props) => {
               isUser && "text-right"
             }`}
           >
-            {new Date(message.created_at).toLocaleString()}
+            <TimeAgo date={new Date(message.created_at)} />
           </p>
         </div>
       </div>
