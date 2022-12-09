7ec775d4813f94cc3ef302b9531106c8640f7558
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-08 21:08:35 -0800

    Update LogoutButton.tsx

--- app/LogoutButton.tsx
+++ app/LogoutButton.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { signOut } from "next-auth/react";
+
 type Props = {};
 
 const LogoutButton = (props: Props) => {
   return (
     <button
-      onClick={() => console.log("logout")}
+      onClick={() => signOut()}
       className="bg-[#74E6DA] hover:bg-[#60beb5] text-white font-bold py-2 px-4 rounded"
     >
       Sign Out
