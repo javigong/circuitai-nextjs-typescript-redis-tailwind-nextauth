a635f06a8938afeab53a2fba9cc83637d62ed488
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-08 21:08:44 -0800

    Create pusher.ts

---
+++ pusher.ts
@@ -0,0 +1,18 @@
+import Pusher from "pusher";
+import ClientPusher from "pusher-js";
+
+export const serverPusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+  useTLS: true,
+});
+
+export const clientPusher = new ClientPusher(
+  process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  {
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    forceTLS: true,
+  }
+);
