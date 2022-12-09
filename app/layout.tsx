b9b09381563c44b3129cade45b28fe227450e080
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-09 11:10:14 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout({
     <html>
       <head />
       <body>
+        {/* @ts-expect-error: Async Server Component */}
         <Header />  
         {children}
       </body>
