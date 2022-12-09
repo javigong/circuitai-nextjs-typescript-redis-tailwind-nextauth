8fac7aea3b6a9df5891d9822610cef4b9f1ada57
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-08 21:08:39 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "googleusercontent.com",
+    ],
+  },
 };
