e6f7ef786dfac26d08d3f335b558133a5d30c057
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-10 11:07:25 -0800

    Update LogoutButton.tsx

--- app/LogoutButton.tsx
+++ app/LogoutButton.tsx
@@ -7,7 +7,7 @@ const LogoutButton = (props: Props) => {
   return (
     <button
       onClick={() => signOut()}
-      className="bg-[#74E6DA] hover:bg-[#60beb5] text-white font-bold py-2 px-4 rounded"
+      className="bg-[#74E6DA] hover:bg-[#60beb5] text-white font-bold text-sm py-2 px-4 rounded"
     >
       Sign Out
     </button>
