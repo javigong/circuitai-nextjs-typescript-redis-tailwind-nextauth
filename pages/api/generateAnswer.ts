56d80e1964ef2c29b53ebde206b7dcc68a76a416
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-10 12:06:56 -0800

    Add more ChatGPT tokens

--- pages/api/generateAnswer.ts
+++ pages/api/generateAnswer.ts
@@ -23,7 +23,7 @@ export default async function handler(
     model: "text-davinci-003",
     prompt: generatePrompt(req.body.question),
     temperature: 0.6,
-    max_tokens: 14,
+    max_tokens: 35,
   });
   res.status(200).json({ result: completion.data.choices[0].text! });
 }
