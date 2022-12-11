30aebd264ffd9619c9e5ff0a474fc2dfca62d512
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-11 15:14:39 -0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,5 +2,6 @@ export { default } from "next-auth/middleware";
 
 // Secure the matching routes with the middleware
 export const config = {
+  runtime: "experimental-edge", // for Edge API Routes only
   matcher: ["/"],
 };
