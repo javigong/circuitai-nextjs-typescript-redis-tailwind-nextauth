ea34e11fd51b05a6d4e09daad96c55cf9a9a232f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-12 15:35:41 -0800

    Update SignInComponent.tsx

--- app/auth/signin/SignInComponent.tsx
+++ app/auth/signin/SignInComponent.tsx
@@ -31,12 +31,7 @@ const SignInComponent = (props: Props) => {
           <div key={provider?.name!}>
             <button
               className="bg-[#74E6DA] hover:bg-[#60beb5] text-white font-bold py-2 px-4 rounded"
-              onClick={() =>
-                signIn(provider?.id!, {
-                  callbackUrl:
-                    process.env.NEXTAUTH_URL,
-                })
-              }
+              onClick={() => signIn(provider?.id!)}
             >
               Sign in with {provider?.name!}
             </button>
