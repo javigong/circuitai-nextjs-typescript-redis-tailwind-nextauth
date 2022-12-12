d2e9a9a14dc3d293ae204f945acacdba410e2cb1
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-11 16:08:47 -0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,5 +2,13 @@ export { default } from "next-auth/middleware";
 
 // Secure the matching routes with the middleware
 export const config = {
+  runtime: "experimental-edge", // for Edge API Routes only
+  unstable_allowDynamic: [
+    "/node_modules/@babel/runtime/regenerator/index.js", // allows a single file
+    "/node_modules/next-auth/core/errors.js",
+    "/node_modules/next-auth/utils/web.js",
+    "/node_modules/next-auth/next/middleware.js",
+    "/node_modules/next-auth/middleware.js",
+  ],
   matcher: ["/"],
 };
