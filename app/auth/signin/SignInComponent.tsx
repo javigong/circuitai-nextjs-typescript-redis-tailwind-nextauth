f90dd9dbae835db76527136e48be1a7d891ffafb
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-12 01:58:33 -0800

    Update SignInComponent.tsx

--- app/auth/signin/SignInComponent.tsx
+++ app/auth/signin/SignInComponent.tsx
@@ -1,29 +1,32 @@
 "use client";
+
 import { getProviders, signIn } from "next-auth/react";
+import { IProviderData } from "../../../types/typings";
 
 type Props = {
-  providers: Awaited<ReturnType<typeof getProviders>> | null;
+  providers: IProviderData[];
 };
 
 const SignInComponent = ({ providers }: Props) => {
-  const providerArray = Object.values(providers!);
 
   return (
     <div className="flex flex-grow justify-center">
-      {providerArray?.map((provider) => (
-        <div key={provider.name}>
-          <button
-            className="bg-[#74E6DA] hover:bg-[#60beb5] text-white font-bold py-2 px-4 rounded"
-            onClick={() =>
-              signIn(provider.id, {
-                callbackUrl: process.env.VERCEL_URL || "http://localhost:3000",
-              })
-            }
-          >
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
+      {providers! &&
+        providers?.map((provider) => (
+          <div key={provider?.name}>
+            <button
+              className="bg-[#74E6DA] hover:bg-[#60beb5] text-white font-bold py-2 px-4 rounded"
+              onClick={() =>
+                signIn(provider?.id, {
+                  callbackUrl:
+                    process.env.VERCEL_URL || "http://localhost:3000",
+                })
+              }
+            >
+              Sign in with {provider?.name}
+            </button>
+          </div>
+        ))}
     </div>
   );
 };
