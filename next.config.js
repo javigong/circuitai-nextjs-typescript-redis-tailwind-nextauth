bc26742471acd7e84536c511e2d232918f85825e
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-12 01:58:34 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   reactStrictMode: true,
   experimental: {
     appDir: true,
