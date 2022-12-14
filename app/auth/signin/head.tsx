6158a175ed4132648169bffd2c3bad1eb479614e
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-14 14:26:46 -0800

    Fix sign in head:  add device-width and initial-scale to 1

--- app/auth/signin/head.tsx
+++ app/auth/signin/head.tsx
@@ -1,7 +1,13 @@
 type Props = {};
 
 const Head = (props: Props) => {
-  return <title>Sign in to CircuitAI</title>;
+  return (
+    <>
+      <title>Sign in to CircuitAI</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
 };
 
 export default Head;
